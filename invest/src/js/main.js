928375a538a9157fdfd3aa5ebed058f993d690c5
filingroove <filingr000ve@gmail.com>
2015-03-30 17:12:16 +0300

    wider collumns

--- invest/src/js/main.js
+++ invest/src/js/main.js
@@ -3,6 +3,8 @@
 $(document).ready(function() {
 		var activity = $('.activity');
 		var button = $('.button');
+		var leftCol = $('#left-col');
+		var rightCol = $('#right-col');
 
 		$('#left-col a.main_link').click(function() {
 			if (activity.hasClass('active')) {
@@ -10,6 +12,8 @@ $(document).ready(function() {
 				if(activity.hasClass('left')) {
 
 					activity.removeClass('active').removeClass('left');
+					leftCol.addClass('pull-left');
+					rightCol.addClass('pull-right');
 
 				} else if(activity.hasClass('right')) {
 					
@@ -17,6 +21,8 @@ $(document).ready(function() {
 				}
 			} else {
 					activity.addClass('active').addClass('left');
+					leftCol.removeClass('pull-left');
+					rightCol.removeClass('pull-right');
 
 			}
 		});
@@ -28,13 +34,18 @@ $(document).ready(function() {
 				if(activity.hasClass('right')) {
 
 					activity.removeClass('active').removeClass('right');
+					leftCol.addClass('pull-left');
+					rightCol.addClass('pull-right');
 
 				} else if(activity.hasClass('left')) {
 					
 					activity.removeClass('left').addClass('right');
+
 				}
 			} else {
-					activity.addClass('active').addClass('right');	
+					activity.addClass('active').addClass('right');
+					leftCol.removeClass('pull-left');
+					rightCol.removeClass('pull-right');
 			}
 		});
 })
